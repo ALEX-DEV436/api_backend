476004ad5e1b250fa14c4b13aca6acb059013632
ALEX-DEV436 <soumaisconquista@gmail.com>
2024-10-31 18:28:02 -0300

    Add files via upload

---
+++ ultimaVersaoFrontEnd/componentes/Informacao.js
@@ -0,0 +1,14 @@
+
+function Informacao(){
+
+  return(  
+<>
+<p>Informação<hr /> </p>
+ <p>  Olá, pesquise o seu personagem da Star Wars e,
+   o inclua na sua lista de personagens favoritos </p> 
+</>
+  
+  )
+
+}
+export default Informacao;
